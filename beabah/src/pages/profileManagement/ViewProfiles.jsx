380e589cc4c8d707958b4602afb5ef9b0d7dca55
Bruno Chim Silveira <101603545+brcsilveira@users.noreply.github.com>
2024-06-12 23:38:03 -0300

    viewProfiles quase pronto, faltando apenas detalhes

---
+++ beabah/src/pages/profileManagement/ViewProfiles.jsx
@@ -0,0 +1,43 @@
+import styles from "../../styles/profileManagement/viewProfiles.module.css";
+import { useEffect, useState } from "react";
+
+export function ViewProfiles() {
+    const [profiles, setProfiles] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProfiles = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/profiles');
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar perfis');
+                }
+                const data = await response.json();
+                console.log('Perfis:', data);
+                setProfiles(data);
+            } catch (error) {
+                setError('Erro ao buscar perfis');
+                console.error('Erro ao buscar perfis:', error);
+            }
+        };
+
+        fetchProfiles();
+    }, []);
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.titulo}>Visualizar Perfil</h1>
+            <ul className={styles.listaPerfis}>
+                {profiles.map(profile => (
+                    <li key={profile.id_perfil} className={styles.itemPerfil}>
+                        <span className={styles.profileName}>{profile.nome_perfil}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
