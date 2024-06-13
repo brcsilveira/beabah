0d2f88fb48fd9a2602aa01a1c9da2c1a9f065fb4
Bruno Chim Silveira <101603545+brcsilveira@users.noreply.github.com>
2024-06-12 23:15:19 -0300

    createProfile está pronto

--- beabah/src/pages/profileManagement/CreateProfilesTF.jsx
+++ beabah/src/pages/profileManagement/CreateProfilesTF.jsx
@@ -126,39 +126,30 @@ export function CreateProfilesTF () {
                                     name='transacao'
                                     value={transacao.id_transacao.toString()}
                                     onChange={handleTransacaoChange} />
-                                    {transacao.nome_transacao}
+                                    {`${transacao.nome_transacao} - ${transacao.descricao}`}
                             </label>
                         )    
                     )) : (
                         <p>Nenhuma transação encontrada.</p>
                     )}
-                    
-                    {/* <label htmlFor="rkpc"><input type="radio" id='rkpc' name='transacao' value={1} checked={formData.transacao === '1'} onChange={handleTransacaoChange}/> Risk Price</label>
-                    <label htmlFor="prfa"><input type="radio" id='prfa' name='transacao' value={2} checked={formData.transacao === '2'} onChange={handleTransacaoChange}/> Produto Fatura</label>
-                    <label htmlFor="defo"><input type="radio" id='defo' name='transacao' value={3} checked={formData.transacao === '3'} onChange={handleTransacaoChange}/> Desconto em Folha</label>
-                    <label htmlFor="pefi"><input type="radio" id='pefi' name='transacao' value={4} checked={formData.transacao === '4'} onChange={handleTransacaoChange}/> Pessoa Física</label>
-                    <label htmlFor="peju"><input type="radio" id='peju' name='transacao' value={5} checked={formData.transacao === '5'} onChange={handleTransacaoChange}/> Pessoa Jurídica</label> */}
                 </div>
                 <h2>Funções</h2>
                 <div className={styles.opcoes}>
-                    {funcoes.map(funcao => (
-                        <label key={funcao.id} htmlFor={`funcao${funcao.id}`}>
-                            <input
-                                type="checkbox"
-                                id={`funcao${funcao.id}`}
-                                name="funcao"
-                                value={funcao.nome}
-                                checked={formData.funcoes.includes(funcao.nome)}
-                                onChange={handleFuncaoChange}
-                            />
-                            {funcao.nome}
-                        </label>
-                    ))}
-
-                    {/* <label><input type="checkbox" name="funcao" value="adicionar" checked={formData.funcoes.includes("adicionar")} onChange={handleFuncaoChange}/> ADAT (Adicionar)</label>
-                    <label><input type="checkbox" name="funcao" value="alterar" checked={formData.funcoes.includes("alterar")} onChange={handleFuncaoChange}/> ALTR (Alterar)</label>
-                    <label><input type="checkbox" name="funcao" value="visualizar" checked={formData.funcoes.includes("visualizar")} onChange={handleFuncaoChange}/> RETR (Visualizar)</label>
-                    <label><input type="checkbox" name="funcao" value="addRestricao" checked={formData.funcoes.includes("addRestricao")} onChange={handleFuncaoChange}/> ADIC (Adicionar Restrição)</label> */}
+                    {funcoes && funcoes.length > 0 ? (
+                        funcoes.map(funcao => (
+                            <label key={funcao.id_funcao} htmlFor={`funcao${funcao.id_funcao}`}>
+                                <input
+                                    type="checkbox"
+                                    id={`funcao${funcao.id_funcao}`}
+                                    name='funcao'
+                                    value={funcao.id_funcao.toString()}
+                                    onChange={handleFuncaoChange} />
+                                    {`${funcao.nome_funcao} - ${funcao.descricao}`}
+                            </label>
+                        )    
+                    )) : (
+                        <p>Nenhuma função encontrada.</p>
+                    )}  
                 </div>
                 <button type="submit" className={styles.criar}>Criar</button>
             </form>
