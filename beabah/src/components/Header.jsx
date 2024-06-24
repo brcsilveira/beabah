809baf0c7da7fa2753619ba4d02091006583abc3
Bruno Chim Silveira <101603545+brcsilveira@users.noreply.github.com>
2024-06-24 09:23:20 -0300

    logout funcionando e estilizado

--- beabah/src/components/Header.jsx
+++ beabah/src/components/Header.jsx
@@ -25,48 +25,48 @@ export function Header() {
                 <div className={styles.header}>
                     <img src={queroqueroLogo} alt="Logo Quero-Quero" />
                     <nav>
-                    <ul className={styles.navList}>
-                        <li>
-                            <Link 
-                            to="/userManagement" 
-                            className={`${styles.navLink} ${location.pathname === '/userManagement' || location.pathname === '/registerUser' || location.pathname === '/editUsers' || location.pathname.startsWith('/editUser') || location.pathname === '/deleteUsers' || location.pathname === '/viewUsers' || location.pathname === '/selectUsers' || location.pathname.startsWith('/linkProfile')  ? styles.active : ''}`}
-                            >
-                            Usuários
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
-                            to="/profileManagement" 
-                            className={`${styles.navLink} ${location.pathname === '/profileManagement' || location.pathname.startsWith('/createProfilesTF')  || location.pathname === '/editProfiles' || location.pathname === '/selectProfiles' || location.pathname === '/deleteProfiles' || location.pathname === '/viewProfiles' || location.pathname.startsWith('/editProfile') || location.pathname.startsWith('/associateModules') ? styles.active : ''}`}
-                            >
-                            Perfis
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
-                            to="/moduleManagement" 
-                            className={`${styles.navLink} ${location.pathname === '/moduleManagement' || location.pathname === '/createModule' || location.pathname.startsWith('/editModule') || location.pathname === '/viewModules' || location.pathname === '/deleteModules' ? styles.active : ''}`}
-                            >
-                            Módulos
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
-                            to="/functionManagement" 
-                            className={`${styles.navLink} ${location.pathname === '/functionManagement' || location.pathname === '/selectModulesF' || location.pathname.startsWith('/createFunction') || location.pathname === '/deleteFunctions' || location.pathname === '/editFunction' || location.pathname.startsWith('/editFunction') || location.pathname === '/viewFunctions' ? styles.active : ''}`}
-                            >
-                            Funções
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
-                            to="/transactionManagement" 
-                            className={`${styles.navLink} ${location.pathname === '/transactionManagement' || location.pathname === '/createTransaction' || location.pathname === '/selectModulesT' || location.pathname === '/viewTransactions' || location.pathname.startsWith('/assignTransactions') || location.pathname.startsWith('/editTransaction') || location.pathname.startsWith('/deleteTransactions') ? styles.active : ''}`}
-                            >
-                            Transações
-                            </Link>
-                        </li>
-                </ul>
+                        <ul className={styles.navList}>
+                            <li>
+                                <Link 
+                                to="/userManagement" 
+                                className={`${styles.navLink} ${location.pathname === '/userManagement' || location.pathname === '/registerUser' || location.pathname === '/editUsers' || location.pathname.startsWith('/editUser') || location.pathname === '/deleteUsers' || location.pathname === '/viewUsers' || location.pathname === '/selectUsers' || location.pathname.startsWith('/linkProfile')  ? styles.active : ''}`}
+                                >
+                                Usuários
+                                </Link>
+                            </li>
+                            <li>
+                                <Link 
+                                to="/profileManagement" 
+                                className={`${styles.navLink} ${location.pathname === '/profileManagement' || location.pathname.startsWith('/createProfilesTF')  || location.pathname === '/editProfiles' || location.pathname === '/selectProfiles' || location.pathname === '/deleteProfiles' || location.pathname === '/viewProfiles' || location.pathname.startsWith('/editProfile') || location.pathname.startsWith('/associateModules') ? styles.active : ''}`}
+                                >
+                                Perfis
+                                </Link>
+                            </li>
+                            <li>
+                                <Link 
+                                to="/moduleManagement" 
+                                className={`${styles.navLink} ${location.pathname === '/moduleManagement' || location.pathname === '/createModule' || location.pathname.startsWith('/editModule') || location.pathname === '/viewModules' || location.pathname === '/deleteModules' ? styles.active : ''}`}
+                                >
+                                Módulos
+                                </Link>
+                            </li>
+                            <li>
+                                <Link 
+                                to="/functionManagement" 
+                                className={`${styles.navLink} ${location.pathname === '/functionManagement' || location.pathname === '/selectModulesF' || location.pathname.startsWith('/createFunction') || location.pathname === '/deleteFunctions' || location.pathname === '/editFunction' || location.pathname.startsWith('/editFunction') || location.pathname === '/viewFunctions' ? styles.active : ''}`}
+                                >
+                                Funções
+                                </Link>
+                            </li>
+                            <li>
+                                <Link 
+                                to="/transactionManagement" 
+                                className={`${styles.navLink} ${location.pathname === '/transactionManagement' || location.pathname === '/createTransaction' || location.pathname === '/selectModulesT' || location.pathname === '/viewTransactions' || location.pathname.startsWith('/assignTransactions') || location.pathname.startsWith('/editTransaction') || location.pathname.startsWith('/deleteTransactions') ? styles.active : ''}`}
+                                >
+                                Transações
+                                </Link>
+                            </li>
+                        </ul>
                     </nav>
                     <div className={styles.user}>
                         <p onClick={toggleDropdown} className={styles.userName}>
