00cd46e25f1e39c0a15e1c318ede9d9f3ed911af
Bruno Chim Silveira <101603545+brcsilveira@users.noreply.github.com>
2024-07-01 11:11:01 -0300

    tela não logada consertada

--- beabah/src/components/ContainerNotLogged.jsx
+++ beabah/src/components/ContainerNotLogged.jsx
@@ -1,16 +1,27 @@
 import { Outlet } from "react-router-dom"
 import queroqueroLogo from '../assets/qq-logo.svg'
 import styles from './ContainerNotLogged.module.css'
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../context/authContext';
 import { Navigate } from 'react-router-dom';
 
 export function ContainerNotLogged() {
     const auth = useContext(AuthContext);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 450);
 
     if (auth.token) {
         return <Navigate to="/userManagement" />
     }
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 450);
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
 
     return (
         <div className={styles.container}>
@@ -21,7 +32,15 @@ export function ContainerNotLogged() {
                 <Outlet />
             </main>
             <footer className={styles.footer}>
-            <p>Lojas Quero-Quero © Todos os direitos reservados</p>
+                {!isMobile && (
+                    <div className={styles.footerDesktop}>
+                    </div>
+                )}
+                {isMobile && (
+                    <div className={styles.footerMobile}>
+                        <p>Lojas Quero-Quero © Todos os direitos reservados</p>
+                    </div>
+                )}
             </footer>
         </div>
     )
